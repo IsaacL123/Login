464b3935b930f3589eaa974d28bec709437486a0
IsaacL123 <126540061+IsaacL123@users.noreply.github.com>
2023-03-13 21:33:17 -0600

    Add files via upload

---
+++ Login/Seguridad.java
@@ -0,0 +1,27 @@
+
+package Login;
+
+public class Seguridad {
+    
+  String encriptar(String pass){
+      
+     
+      
+      char array[] = pass.toCharArray();
+      
+      for(int i = 0; i < array.length; i++){
+            array[i] = (char)(array[i] + (char)307);
+      
+      }
+      String encriptado = String.valueOf(array);
+  
+      System.out.println(encriptado);
+      
+     return encriptado;         
+              
+  }
+  
+   
+    
+   
+}
